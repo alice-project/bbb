06f0cbd249a115c3fbf2083ee04831aab28cbcd7
Beaglebone Black <bbb@HP-6910p.(none)>
2014-04-21 22:09:06 +0800

    rename hamster to hm

--- hamster/r_commu.h
+++
@@ -1,7 +0,0 @@
-#ifndef __R_COMMU_H__
-#define __R_COMMU_H__
-
-void *communation(void *data);
-
-#endif
-
--- hamster/r_led.h
+++
@@ -1,8 +0,0 @@
-#ifndef __R_LED_H__
-#define __R_LED_H__
-
-int led_blink(void *);
-int led_regist();
-
-#endif
-
